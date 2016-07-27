45655decd59198863aa38899bdaf9cea420bfebc
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2016-07-27 18:17:17 +0200

    #! create aliases

--- src/__tests__/mainSpec.js
+++ src/__tests__/mainSpec.js
@@ -6,8 +6,20 @@ describe('UIAtoms', () => {
 		expect(UIAtoms.Headline).not.toEqual(null);
 	});
 
+	it('should have the headline1 component', () => {
+		expect(UIAtoms.Headline1).not.toEqual(null);
+	});
+
+	it('should have the headline1 alias as H1', () => {
+		expect(UIAtoms.H1).not.toEqual(null);
+	});
+
 	it('should have the image component', () => {
 		expect(UIAtoms.Image).not.toEqual(null);
+	});
+
+	it('should have the image alias as img', () => {
+		expect(UIAtoms.Img).not.toEqual(null);
 	});
 
 	it('should have the iframe component', () => {
@@ -16,6 +28,10 @@ describe('UIAtoms', () => {
 
 	it('should have the link component', () => {
 		expect(UIAtoms.Link).not.toEqual(null);
+	});
+
+	it('should have the link alias as A', () => {
+		expect(UIAtoms.A).not.toEqual(null);
 	});
 
 	it('should have the loader component', () => {
@@ -30,11 +46,27 @@ describe('UIAtoms', () => {
 		expect(UIAtoms.Paragraph).not.toEqual(null);
 	});
 
+	it('should have the paragraph alias as P', () => {
+		expect(UIAtoms.P).not.toEqual(null);
+	});
+
 	it('should have the list component', () => {
 		expect(UIAtoms.List).not.toEqual(null);
 	});
 
-	it('should have the li component', () => {
+	it('should have the unordered list component', () => {
+		expect(UIAtoms.UnorderedList).not.toEqual(null);
+	});
+
+	it('should have the unordered list alias as ul', () => {
+		expect(UIAtoms.Ul).not.toEqual(null);
+	});
+
+	it('should have the listItem component', () => {
+		expect(UIAtoms.ListItem).not.toEqual(null);
+	});
+
+	it('should have the listItem alias as Li', () => {
 		expect(UIAtoms.Li).not.toEqual(null);
 	});
 
--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
 import UIComponentHelper from './UIComponentHelper';
 
 import Headline from './headline/Headline';
-import H1 from './headline/H1';
-import H2 from './headline/H2';
-import H3 from './headline/H3';
-import H4 from './headline/H4';
-import H5 from './headline/H5';
-import H6 from './headline/H6';
+import Headline1 from './headline/Headline1';
+import Headline2 from './headline/Headline2';
+import Headline3 from './headline/Headline3';
+import Headline4 from './headline/Headline4';
+import Headline5 from './headline/Headline5';
+import Headline6 from './headline/Headline6';
 
 import Iframe from './iframe/Iframe';
 
 import Image from './image/Image';
 
-import Li from './li/Li';
-
 import Link from './link/Link';
 
 import List from './list/List';
+import OrderedList from './list/OrderedList';
+import UnorderedList from './list/UnorderedList';
+import ListItem from './list/ListItem';
 
 import Loader from './loader/Loader';
 
@@ -33,7 +34,6 @@ let $registerImaPlugin = (ns) => {
 	ns.namespace('ima.ui.atom.headline');
 	ns.namespace('ima.ui.atom.iframe');
 	ns.namespace('ima.ui.atom.image');
-	ns.namespace('ima.ui.atom.li');
 	ns.namespace('ima.ui.atom.link');
 	ns.namespace('ima.ui.atom.list');
 	ns.namespace('ima.ui.atom.loader');
@@ -41,27 +41,43 @@ let $registerImaPlugin = (ns) => {
 	ns.namespace('ima.ui.atom.sizer');
 	ns.namespace('ima.ui.atom.video');
 
-	ns.ima.ui.atom.headline.Headline = Headline;
-	ns.ima.ui.atom.headline.H1 = H1;
-	ns.ima.ui.atom.headline.H2 = H2;
-	ns.ima.ui.atom.headline.H3 = H3;
-	ns.ima.ui.atom.headline.H4 = H4;
-	ns.ima.ui.atom.headline.H5 = H5;
-	ns.ima.ui.atom.headline.H6 = H6;
+	let nsHeadline = ns.ima.ui.atom.headline;
+
+	nsHeadline.Headline = Headline;
+	nsHeadline.H1 = H1;
+	nsHeadline.H2 = H2;
+	nsHeadline.H3 = H3;
+	nsHeadline.H4 = H4;
+	nsHeadline.H5 = H5;
+	nsHeadline.H6 = H6;
+	nsHeadline.Headline1 = H1;
+	nsHeadline.Headline2 = H2;
+	nsHeadline.Headline3 = H3;
+	nsHeadline.Headline4 = H4;
+	nsHeadline.Headline5 = H5;
+	nsHeadline.Headline6 = H6;
 
 	ns.ima.ui.atom.iframe.Iframe = Iframe;
 
 	ns.ima.ui.atom.image.Image = Image;
+	ns.ima.ui.atom.image.Img = Image;
 
 	ns.ima.ui.atom.link.Link = Link;
+	ns.ima.ui.atom.link.A = Link;
 
-	ns.ima.ui.atom.li.Li = Li;
-
-	ns.ima.ui.atom.list.List = List;
+	nsList = ns.ima.ui.atom.list;
+	nsList.List = List;
+	nsList.ListItem = ListItem;
+	nsList.OrderedList = OrderedList;
+	nsList.UnorderedList = UnorderedList;
+	nsList.Li = ListItem;
+	nsList.Ol = OrderedList;
+	nsList.Ul = UnorderedList;
 
 	ns.ima.ui.atom.loader.Loader = Loader;
 
 	ns.ima.ui.atom.paragraph.Paragraph = Paragraph;
+	ns.ima.ui.atom.paragraph.P = Paragraph;
 
 	ns.ima.ui.atom.sizer.Sizer = Sizer;
 
@@ -79,19 +95,33 @@ export {
 	UIComponentHelper,
 	defaultDependencies,
 	Headline,
-	H1,
-	H2,
-	H3,
-	H4,
-	H5,
-	H6,
+	Headline1,
+	Headline2,
+	Headline3,
+	Headline4,
+	Headline5,
+	Headline6,
+	Headline1 as H1,
+	Headline2 as H2,
+	Headline3 as H3,
+	Headline4 as H4,
+	Headline5 as H5,
+	Headline6 as H6,
 	Iframe,
 	Image,
+	Image as Img,
 	Link,
-	Li,
+	Link as A,
 	List,
+	ListItem,
+	OrderedList,
+	UnorderedList,
+	ListItem as Li,
+	OrderedList as Ol,
+	UnorderedList as Ul,
 	Loader,
 	Paragraph,
+	Paragraph as P,
 	Sizer,
 	Video,
 	initBind,
