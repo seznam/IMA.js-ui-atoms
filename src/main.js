07ef113132d65fbfe7e37bd76e2e0fa7955f999f
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-07-28 11:23:13 +0200

    bugfix - undeclared variable

--- src/main.js
+++ src/main.js
@@ -65,7 +65,7 @@ let $registerImaPlugin = (ns) => {
 	ns.ima.ui.atom.link.Link = Link;
 	ns.ima.ui.atom.link.A = Link;
 
-	nsList = ns.ima.ui.atom.list;
+	var nsList = ns.ima.ui.atom.list;
 	nsList.List = List;
 	nsList.ListItem = ListItem;
 	nsList.OrderedList = OrderedList;
