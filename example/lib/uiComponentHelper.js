3c799df75084500a1c6d23412fa327004c0cef62
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2017-05-11 17:04:31 +0200

    # the classnames module was added

--- example/lib/uiComponentHelper.js
+++ example/lib/uiComponentHelper.js
@@ -2,7 +2,8 @@ import { UIComponentHelper } from '../../dist/main.js';
 import componentPositions from './componentPositions';
 import _router from './routerMock';
 import visibility from './visibility';
+import classnames from 'classnames';
 
-let uiComponentHelper = new UIComponentHelper(_router, componentPositions, visibility);
+let uiComponentHelper = new UIComponentHelper(_router, componentPositions, visibility, classnames);
 
 export default uiComponentHelper;
