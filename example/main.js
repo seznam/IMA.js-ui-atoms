6ea16652a910c3e8763adb6e62d2ca1a9e4e87fe
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2017-08-03 12:18:49 +0200

    #! udpate example

--- example/main.js
+++ example/main.js
@@ -19,69 +19,83 @@ window.Atoms = Atoms;
 		width = {600}
 		height = {400}/>*/
 
+let imageUrl1 = 'https://static.pexels.com/photos/279315/pexels-photo-279315.jpeg';
+let imageUrl2 = 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg';
+
+let { Image, Paragraph, H1, Link, List, ListItem } = Atoms;
+
 ReactDOM.render(
 	<ParentComponent>
-		<Atoms.Paragraph style = {{ width: 600 }}>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</Atoms.Paragraph>
-		<p style = {{ width: 600 }}>
+		</Paragraph>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<p style = {{ width: 600 }}>
+		</Paragraph>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<Atoms.Image
-				src = 'https://images.pexels.com/photos/3247/nature-forest-industry-rails.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
+		</Paragraph>
+		<Image
+				src = { imageUrl1 }
 				width = {600}
 				height = {400}/>
-		<p style = {{ width: 600 }}>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<p style = {{ width: 600 }}>
+		</Paragraph>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<Atoms.Image
-				src = 'https://images.pexels.com/photos/3247/nature-forest-industry-rails.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
+		</Paragraph>
+		<Image
+				src = { imageUrl1 }
 				width = {600}
 				height = {400}/>
-		<p style = {{ width: 600 }}>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<p style = {{ width: 600 }}>
+		</Paragraph>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<Atoms.Image
-				src = 'https://images.pexels.com/photos/3247/nature-forest-industry-rails.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
+		</Paragraph>
+		<Image
+				src = { imageUrl1 }
 				width = {600}
 				height = {400}/>
-		<p style = {{ width: 600 }}>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<p style = {{ width: 600 }}>
+		</Paragraph>
+		<Paragraph style = {{ width: 600 }}>
 			Lorem Ipsum je demonstrativní výplňový text používaný v tiskařském a knihařském průmyslu. Lorem Ipsum je považováno za standard v této oblasti už od začátku 16. století, kdy dnes neznámý tiskař vzal kusy textu a na jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz nevydržel pouze pět století, on přežil i nástup elektronické sazby v podstatě beze změny. Nejvíce popularizováno bylo Lorem Ipsum v šedesátých letech 20. století, kdy byly vydávány speciální vzorníky s jeho pasážemi a později pak díky počítačovým DTP programům jako Aldus PageMaker.
-		</p>
-		<Atoms.Image
-				src = 'https://images.pexels.com/photos/3247/nature-forest-industry-rails.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
+		</Paragraph>
+		<H1>
+			Web imajs.io
+		</H1>
+		<List>
+			<ListItem mode = 'disc'>listItem 1</ListItem>
+			<ListItem mode = 'circle'>listItem 2</ListItem>
+			<ListItem mode = 'square'>listItem 3</ListItem>
+		</List>
+		<Link href = 'https://imajs.io' target = '_blank'>imajs.io</Link>
+		<Image
+				src = { imageUrl1 }
 				width = {600}
 				height = {400}/>
-		<Atoms.Image
-				src = 'https://images.pexels.com/photos/3247/nature-forest-industry-rails.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
+		<Image
+				src = { imageUrl1 }
 				width = {600}
 				height = {400}/>
-		<Atoms.Image
-				src = 'https://images.pexels.com/photos/3247/nature-forest-industry-rails.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
+		<Image
+				src = { imageUrl1 }
 				width = {600}
 				height = {400}/>
-		<Atoms.Image
-				src = 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'
+		<Image
+				src = { imageUrl2 }
 				width = {600}
 				height = {400}/>
-		<Atoms.Image
-				src = 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'
+		<Image
+				src = { imageUrl2 }
 				width = {600}
 				height = {400}/>
-		<Atoms.Image
-				src = 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'
+		<Image
+				src = { imageUrl2 }
 				width = {600}
 				height = {400}/>
 	</ParentComponent>,
